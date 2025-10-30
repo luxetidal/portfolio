196310630772224cd4a6b63e31c7f39f3d1fb589
luxetidal <luxetidal@gmail.com>
2025-10-31 01:30:35 +0900

    make the project page

---
+++ app/components/utils/ProjectImage.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Blurhash } from "react-blurhash";
+
+interface ProjectImageProps {
+  image: string;
+  blurhash: string;
+  alt: string;
+}
+
+const ProjectImage: React.FC<ProjectImageProps> = ({
+  image,
+  blurhash,
+  alt,
+}) => {
+  const [loaded, setLoaded] = useState(false);
+  return (
+    <div className="relative h-60 w-full overflow-hidden rounded-xl">
+      {!loaded && (
+        <Blurhash
+          hash={blurhash}
+          width="100%"
+          height="100%"
+          resolutionX={32}
+          resolutionY={32}
+          punch={1}
+          className="absolute top-0 left-0 h-full w-full"
+        />
+      )}
+      <img
+        src={image}
+        onLoad={() => setLoaded(true)}
+        className={`absolute top-0 left-0 h-full w-full object-cover rounded-xl transition-opacity duration-500 ${
+          loaded ? "opacity-100" : "opacity-0"
+        }`}
+        alt={alt}
+      />
+    </div>
+  );
+};
+export default ProjectImage;
